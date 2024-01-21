84415e004417e3b5958a5ba9eb89829e98391cd8
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-01-21 14:34:19 -0500

    battery alerts: add a third optional argument
      If present, a quieter version of both audio files is played.

--- battery-notifications.sh
+++ battery-notifications.sh
@@ -7,6 +7,14 @@
 # the first lower than the second.
 # These are the percentages you would like the battery
 # to remain between.
+# If any third argument is passed,
+# the quiet version of the file is used.
+
+if [[ $# -gt 2 ]]; then
+    suffix="-quiet"
+else
+    suffix=""
+fi
 
 while true; do
   acpi_message=`acpi`
@@ -16,15 +24,17 @@ while true; do
      [[ $acpi_message = *'Discharging'* ]]; then
     notify-send --urgency=CRITICAL \
                   "Battery Low (${battery_level}%) and discharging"
-    aplay -q ~/Audio/battery-low.wav
-    sleep 300 # to prevent redundant notifications
+    aplay -q ~/Audio/battery-low$suffix.wav
+    echo    "~/Audio/battery-low$suffix.wav"
+    sleep 180 # to prevent redundant notifications
 
   elif [ $battery_level -ge $2          ] && \
        [[ $acpi_message = *' Charging'* ]]; then
     notify-send --urgency=CRITICAL \
                   "Battery High (${battery_level}%) and charging"
-    aplay -q ~/Audio/battery-high.wav
-    sleep 300 # to prevent redundant notifications
+    aplay -q ~/Audio/battery-high$suffix.wav
+    echo    "~/Audio/battery-high$suffix.wav"
+    sleep 180 # to prevent redundant notifications
   fi
 
   sleep 60
