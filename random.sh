d58cfadbfc39d1927b23886e4509d63685aab886
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-10-06 20:39:20 -0500

    drop capital letters from random.sh

--- random.sh
+++ random.sh
@@ -1,6 +1,10 @@
 # Takes a numerical argument: the number of characters you want.
 
+# PITFALL: This used to give capital letters too.
+# I think it's easier to remember the password without those.
+# To include them, insert A-Z after the a-z (sans whitespace).
+
 # What is this?
 #!/run/current-system/sw/bin/bash
 
-head /dev/urandom | tr -dc A-Za-z0-9 | head -c $1 ; echo ''
+head /dev/urandom | tr -dc a-z0-9 | head -c $1 ; echo ''
