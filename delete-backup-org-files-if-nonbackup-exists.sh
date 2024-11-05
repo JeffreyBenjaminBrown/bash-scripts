ed911269865f1df4007ff28481f258ebc3551a88
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-11-05 11:08:58 -0500

    more

---
+++ delete-backup-org-files-if-nonbackup-exists.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Find all .org files in the directory and subdirectories
+find . -type f -name "*.org" | while read org_file; do
+  # Check if a file with the same name ending in ~ exists
+  backup_file="${org_file}~"
+  if [ -e "$backup_file" ]; then
+    # Print the backup file name
+    rm "$backup_file"
+  fi
+done
