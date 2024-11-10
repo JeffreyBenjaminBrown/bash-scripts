68822ae6f964b77efc5bfec84dd708b9d48655d0
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-11-10 08:11:00 -0500

    more

--- du-home-for-borg.sh
+++ du-home-for-borg.sh
@@ -1,6 +1,7 @@
 # Based on du-biggest-recursive.sh,
 # which is *almost* more general, but it excludes nothing.
-du -a /home/jeff                                        \
+du -a -Bk                                               \
+    /home/jeff                                          \
      --exclude-from=/home/jeff/bin/exclude-for-borg.txt \
     | sort -n -r                                        \
     | head -n 300
---
+++ my-nixos-rebuild.sh
@@ -0,0 +1,1 @@
+sudo bash -c "nix-channel --update nixos && nixos-rebuild switch"
