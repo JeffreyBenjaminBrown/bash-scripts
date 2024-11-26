f2aeaee942d299db0e70c7edb83a9e26f60c6970
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-11-26 17:43:54 -0500

    comments

--- du-home-for-borg.sh
+++ du-home-for-borg.sh
@@ -1,3 +1,12 @@
+# PITFALL: The $EXCLUDE file cannot contain empty lines,
+# not even as the last line.
+# `mg` lets me do that; Emacs does not.
+# I tried for more than an hour, I think,
+# to leave it formatted the way I wanted
+# and instead strip the whitespace with shell magic,
+# but could not manage it.
+# Part of the problem is that (>) in Bash appends a newline.
+
 EXCLUDE=/home/jeff/bin/exclude-for-borg.txt
 OUTPUT=/home/jeff/org-roam/pers/memory_used_in_home_folder.org
 
