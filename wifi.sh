d85767742c87183b288749b5b0a90e2a310028a6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-06-25 14:23:43 -0500

    more

---
+++ wifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# WiFi connection script
+# Usage: wifi [jeff|mincit]
+
+case "$1" in
+    "jeff")
+        nmcli connection up "Jeff's Galaxy"
+        echo "can't do cargo (rust)"
+        ;;
+    "mincit")
+        nmcli connection up WIFI_MOVILES_MINCIT
+        echo "can ssh to github,"
+        echo "can't do cargo (rust)"
+        echo "can't do apt (ubuntu, debian)"
+        ;;
+    *)
+        echo "Usage: wifi.sh [jeff|mincit]"
+        echo "  jeff   - Connect to Jeff's Galaxy"
+        echo "  mincit - Connect to WIFI_MOVILES_MINCIT"
+        exit 1
+        ;;
+esac
